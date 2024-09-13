68a12cafb7fb8ff4d7b8b012c769a937ee68eb49
Edgar <businessmec@yahoo.com>
2024-09-13 17:07:18 +0200

    object creation there

--- src/main/java/com/echandens/kafka/producer/KafkaEchandensProducer.java
+++ src/main/java/com/echandens/kafka/producer/KafkaEchandensProducer.java
@@ -1,6 +1,8 @@
 package com.echandens.kafka.producer;
 
 
+import com.echandens.kafka.model.Contact;
+import com.echandens.kafka.model.Score;
 import com.echandens.kafka.model.Student;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +11,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -25,12 +28,25 @@ public class KafkaEchandensProducer {
     private KafkaTemplate<String, Student> kafkaTemplate;
 
     public void sendStudent(Student student) {
+        Contact contact = Contact
+                .newBuilder()
+                .setEmailId("jean@jean.ch")
+                .setMobile("0671028580")
+                .build();
+
+        Score score =
+                Score.newBuilder().
+                        setMarks(18)
+                        .setSubject("je suis la")
+                        .build();
         student = Student.newBuilder().
                 setFirstName("Jean").
                 setId(UUID.randomUUID().toString()).
                 setRollNumber(10).
                 setLastName("jouarre").
                 setClassName("installation").
+                setScores(Collections.singletonList(score))
+                        .setContact(contact).
                 build();
         log.info("transfert objet student firstname {} lastname {} inputTopic {} , outputTopic {}", student.getFirstName(),
                 student.getLastName(), inputTopic, outputTopic);
